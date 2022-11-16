2eceb2250e616761a37ea713402c93d54c51b32a
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-11-16 23:39:08 +0200

    Abdo GetTests  and GetCost Func

--- health_care_center_management_system/Diagnosis.cs
+++ health_care_center_management_system/Diagnosis.cs
@@ -18,6 +18,19 @@ namespace health_care_center_management_system
             InitializeComponent();
             Con = new Functions();
             Getpatients();
+            GetTests();
+
+        }
+
+        private void GetCost()
+        {
+            string Query = "select * from TestTbl Where TestCode = {0}";
+            Query = string.Format(Query, TestCb.SelectedValue.ToString());
+            foreach(DataRow dr in Con.GetData(Query).Rows)
+            {
+                CostTb.Text = dr["TestCost"].ToString();
+
+            }
         }
         private void Getpatients()
         {
@@ -28,10 +41,25 @@ namespace health_care_center_management_system
 
 
         }
+        private void GetTests()
+        {
+            string Query = "Select * from TestTbl";
+            TestCb.DisplayMember = Con.GetData(Query).Columns["TestName"].ToString();
+            TestCb.ValueMember = Con.GetData(Query).Columns["TestCode"].ToString();
+            TestCb.DataSource = Con.GetData(Query);
+
+
+        }
 
         private void SaveBtn_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void TestCb_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            GetCost();
+       
         }
     }
 }
